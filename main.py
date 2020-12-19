0366f8d7f84c4c6bd17ad5cf914bd1fb9daf6008
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2020-12-19 15:42:28 -0500

    calculate volumn depend on shape, store result in list of tuple

--- main.py
+++ main.py
@@ -12,4 +12,25 @@ def main():
 		while shape not in VALIDINPUT:
 			shape = input("		-- invalid input: enter shape (quit/q, cube/c, pyramid/p, ellipsoid/e) Please enter shape: ").lower()
 
+		if shape in ['cube', 'c']:
+			side = float(input("Enter length of side for the cube: "))
+			results.append(("cube", volume.cube(side)))
+			print("The volume of a cube with side {} is: {:.2f}\n".format(side, results[-1][1]))
+
+		elif shape in ['pyramid', 'p']:
+			base = float(input("Enter the base of the pyramid: "))
+			height = float(input("Enter the height of the pyramid: "))
+			results.append(("pyramid", volume.pyramid(base, height)))
+			print("The volume of a pyramid with base {} and height {} is: {:.2f}\n".format(base, height, results[-1][1]))
+
+		elif shape in ['ellipsoid', 'e']:
+			r1 = float(input("Enter the first radius: "))
+			r2 = float(input("Enter the second radius: "))
+			r3 = float(input("Enter the third radius: "))
+			results.append(("ellipsoid", volume.ellipsoid(r1, r2, r3)))
+			print("The volume of an ellipsoid with radii {} and {} and {} is: {:.2f}\n".format(r1, r2, r3, results[-1][1]))
+
+		else:
+			break
+
 main()
